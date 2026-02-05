f384182fad344def93a191cb66425322395329ba
Victor Tapia <victortapiaegana@gmai.com>
2026-02-05 14:59:15 -0300

    ADD:Logica de validacion de nombre

--- funciones/crear_producto.py
+++ funciones/crear_producto.py
@@ -2,8 +2,26 @@ from ui.ventana import ventana
 
 def crear_registro()->none:
 
+    nombre:str = ''
+
     ventana(["Sistema de inventario",
              "",
              "CREAR NUEVO REGISTRO"])
 
-    nombre = input("Nombre del producto: ")
+    while not nombre or nombre.isspace():
+        """ validacio del nombre del producto"""
+        
+        nombre = input("Nombre del producto: ")
+
+        if nombre.isdigit():
+            print("El nombre no puede ser un numero")
+            nombre = None
+        elif not nombre or nombre.isspace():
+            print("El nombre no puede estar vacio")
+            nombre = None
+
+
+
+
+    input("PRODUCTO CREADO!!!!! \nPresione cualquier tecla para continuar...")
+
